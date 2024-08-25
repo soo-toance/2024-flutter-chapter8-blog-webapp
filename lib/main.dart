d71c0535212aeae8db635779da6b874702e50ca0
may <soo.toance@gmail.com>
2024-08-25 18:01:56 +0900

    widgetsflutterbinding.ensureinitialized 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:chapter8_blog_webapp/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  // 플러터 프레임워크가 앱을 실행할 준비가 될 때까지 기다림
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     MaterialApp(
       home: HomeScreen(),
