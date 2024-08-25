30c03838d781dcf00fa1b14970fbbe8b97e3c871
may <soo.toance@gmail.com>
2024-08-25 17:55:25 +0900

    앱바 구현하기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
+  WebViewController webViewController = WebViewController();
 
-  // const 생성자
-  const HomeScreen({Key? key}) : super(key: key);
+  // webviewcontroller가 non-const 이기 때문에 삭제
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home screen'),
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Code Factory'),
+        centerTitle: true,
+      ),
+      body: Text('Home Screen'),
     );
   }
 }
